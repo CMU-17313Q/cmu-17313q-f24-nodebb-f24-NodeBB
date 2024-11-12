7e7aa059c8a82530deddcb9dcf4d966069738ce8
Ghani <ganiraisov@gmail.com>
2024-11-12 16:04:38 +0300

    lint fixes
    lint fixes

--- src/translate/index.js
+++ src/translate/index.js
@@ -1,12 +1,13 @@
+'use strict';
 
-var request = require('request');
+const request = require('request');
 
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
-    // Edit the translator URL below
-    const TRANSLATOR_API = "https://nodebb-f24-translator.azurewebsites.net/"
-    const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
-    const data = await response.json();
-    return [data["is_english"], data["translated_content"]]
-}
+	// Edit the translator URL below
+	const TRANSLATOR_API = 'https://translator-swifites.azurewebsites.net/';
+	const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
+	const data = await response.json();
+	return [data.is_english, data.translated_content];
+};
